1e232ea9bb500c6204f38076bb7f384635610966
Maycon Clemente Caboi <caboi.cmike@gmail.com>
2019-07-15 10:35:26 -0300

    EMERGENCY SOUND

--- Assets/Script/Rocket.cs
+++ Assets/Script/Rocket.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -24,6 +27,11 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
+            
         }
 
         if (Input.GetKey(KeyCode.A))
